d838913089cecb8566255ae895f1dcd1cb286295
chanchana <north1602@gmail.com>
2022-03-05 18:26:53 +0700

    Fix mobile scroll offset

--- src/hooks/useNavigation.tsx
+++ src/hooks/useNavigation.tsx
@@ -2,11 +2,15 @@ import React from "react"
 import { useSearchParams } from "react-router-dom";
 import { Parameter } from "../constants/parameter";
 import { NavigationContext } from "../contexts/navigation";
+import { ResponsiveContext } from "../contexts/responsive";
 import { useOnScreen } from "./useOnScreen";
 
 export const useNavigation = (ref: React.RefObject<HTMLDivElement>, path: string) => {
     const { routePath, setRoutePath, newRoutePath, setNewRoutePath } = React.useContext(NavigationContext);
+    const { isMobile } = React.useContext(ResponsiveContext)
     const isOnScreen = useOnScreen(ref)
+    
+    const scrollOffset = isMobile ? -88 : -120
 
     React.useEffect(() => {
         if (isOnScreen) {
@@ -16,7 +20,7 @@ export const useNavigation = (ref: React.RefObject<HTMLDivElement>, path: string
 
     React.useEffect(() => {
         if (newRoutePath !== '' && newRoutePath === path && ref.current) {
-            window.scrollTo({ behavior: 'smooth', top: ref.current.offsetTop - 120 })
+            window.scrollTo({ behavior: 'smooth', top: ref.current.offsetTop + scrollOffset })
             if (setRoutePath && setNewRoutePath) {
                 setRoutePath(newRoutePath)
                 setNewRoutePath('')
