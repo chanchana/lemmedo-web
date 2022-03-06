090d5f84fcb2c93f69b4a15be2bc32a715f9d744
chanchana <north1602@gmail.com>
2022-03-06 16:49:12 +0700

    Try remove form tag

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -4,13 +4,11 @@ import { Style } from "../styles/style"
 
 export const ContactForm = () => {
     return (
-        <form>
-            <Stack vertical gap={Style.Spacing.L}>
-                <TextField label={Content.Label.Name} placeholder={Content.Label.NamePlaceholder}/>
-                <TextField label={Content.Label.Email} placeholder={Content.Label.EmailPlaceholder}/>
-                <TextArea label={Content.Label.Message} placeholder={Content.Label.MessagePlaceholder}/>
-                <Button large backgroundColor={Style.Gradient.GreenToCyan}>{Content.Action.SendMessage}</Button>
-            </Stack>
-        </form>
+        <Stack vertical gap={Style.Spacing.L}>
+            <TextField label={Content.Label.Name} placeholder={Content.Label.NamePlaceholder}/>
+            <TextField label={Content.Label.Email} placeholder={Content.Label.EmailPlaceholder}/>
+            <TextArea label={Content.Label.Message} placeholder={Content.Label.MessagePlaceholder}/>
+            <Button large backgroundColor={Style.Gradient.GreenToCyan}>{Content.Action.SendMessage}</Button>
+        </Stack>
     )
 }
