a096961ad6a081ae806725317137ca83e27c7ef2
chanchana <north1602@gmail.com>
2022-03-21 21:53:42 +0700

    Add project captions

--- src/data/showcases.ts
+++ src/data/showcases.ts
@@ -35,15 +35,15 @@ export interface Link {
     label?: string,
 }
 
-const longString = 'Lorem ipsum dolor sit amet, consecte amet, consectetur adipiscing elit. Phasellus pretiumng elit. Phndum. Pellentesque ac. dolor sit amet, consectetur adipiscing elit. Phasellus pretium a mi et bibendum. Pellentesque ac.'
+const unavailableText = 'The detail will be added soon.'
 
 const pordeetorjai: ShowcaseData = {
     title: 'Pordeetorjai Personal Blog',
-    caption: longString,
+    caption: "A responsive personal blog website that introduces a Japanese-influenced marketing and organization concept. The website gives you a warm and calm sensation. Por-dee-tor-jai means satisfied in Thai.",
     imageUrl: PordeetorjaiSrc,
     categories: [Category.Web],
 
-    details: [longString, longString],
+    details: [unavailableText],
     tools: ['TypeScript', 'React', 'Frontity', 'Wordpress API'],
     images: [
         { label: 'Pordeetorjai', imageUrl: PordeetorjaiSrc },
@@ -70,11 +70,11 @@ const pordeetorjai: ShowcaseData = {
 
 const valdb: ShowcaseData = {
     title: 'ValDB, Validation Report Database System',
-    caption: longString,
+    caption: "A new improved version of report database system in CERN, the international organization. The new version features UX/UI improvements, new essential features, and modern development technologies.",
     imageUrl: ValdbSrc,
     categories: [Category.Web],
 
-    details: [longString, longString],
+    details: [unavailableText],
     tools: ['TypeScript', 'React', 'Python', 'MongoDB', 'Github Actions'],
     images: [
         { label: 'ValDB', imageUrl: ValdbSrc },
@@ -98,11 +98,11 @@ const valdb: ShowcaseData = {
 
 const restaurantRecommendation: ShowcaseData = {
     title: 'Restaurant Recommender for Individuals and Groups',
-    caption: longString,
+    caption: "A web application that helps your group decide where to eat more easily. It provides over ten thousand restaurants in Bangkok. This application uses a customized genetic algorithm and a rank aggregation algorithm to provide the most suitable restaurant for groups.",
     imageUrl: RestaurantRecommendationSrc,
     categories: [Category.Web, Category.App, Category.Ai],
 
-    details: [longString, longString],
+    details: [unavailableText],
     tools: ['TypeScript', 'React', 'ExpressJS', 'Python', 'MongoDB', 'Google Cloud'],
     images: [
         { label: 'Restaurant Recommender for Individuals and Groups', imageUrl: RestaurantRecommendationSrc },
@@ -126,11 +126,11 @@ const restaurantRecommendation: ShowcaseData = {
 
 const musicAi: ShowcaseData = {
     title: 'Emotion-based Music Generation',
-    caption: longString,
+    caption: "This is one of my university’s AI projects. The MIDI files, including sad, happy, and clam music, were prepared, and the model was constructed using the LSTM architecture. The result is decently generated music for each emotion.",
     imageUrl: MusicAiSrc,
     categories: [Category.Ai],
 
-    details: [longString, longString],
+    details: [unavailableText],
     tools: ['Python', 'Keras'],
     images: [
         { label: 'Emotion-based Music Generation', imageUrl: MusicAiSrc },
@@ -156,12 +156,12 @@ const musicAi: ShowcaseData = {
 }
 
 const mockMobileBanking: ShowcaseData = {
-    title: 'Mobile Banking System Simulation',
-    caption: longString,
+    title: 'Online Banking System Simulation',
+    caption: "This is a part of the university’s database class project. The project simulates online banking behavior. It includes the mobile website that can be used for transactions and the desktop website for managers and bank officers to see the dashboards.",
     imageUrl: MockMobileBankSrc,
     categories: [Category.Web],
 
-    details: [longString, longString],
+    details: [unavailableText],
     tools: ['JavaScript', 'Vue', 'Python', 'MySQL'],
     images: [
         { label: 'Mobile Banking System Simulation', imageUrl: MockMobileBankSrc },
@@ -185,11 +185,11 @@ const mockMobileBanking: ShowcaseData = {
 
 const rush: ShowcaseData = {
     title: 'RUSH, Store Navigation',
-    caption: longString,
+    caption: "A web application that aims to reduce the time used in stores during rush hours. It can quickly calculate the shortest route, so you don’t have to endlessly walk around the store. It uses the nearest neighbor algorithm to find the shortest routes given any item's location.",
     imageUrl: RushSrc,
     categories: [Category.Web],
 
-    details: [longString, longString],
+    details: [unavailableText],
     tools: ['JavaScript', 'Vue', 'Python', 'MySQL'],
     images: [
         { label: 'RUSH', imageUrl: RushSrc },
@@ -213,11 +213,11 @@ const rush: ShowcaseData = {
 
 const taxi: ShowcaseData = {
     title: 'Taxi, Carpool Application',
-    caption: longString,
+    caption: "This project is a part of the university’s software engineering project. It is a carpool application that lets the university’s students and staff share the cost of public transportation, such as taxis. It features an easy-to-use user interface and a convenient in-app chat system.",
     imageUrl: TaxiSrc,
     categories: [Category.Web],
 
-    details: [longString, longString],
+    details: [unavailableText],
     tools: ['JavaScript', 'Vue', 'Python', 'MongoDB'],
     images: [
         { label: 'Taxi', imageUrl: TaxiSrc },
@@ -241,11 +241,11 @@ const taxi: ShowcaseData = {
 
 const fost: ShowcaseData = {
     title: 'Fost, Lost and found, Item Finder Application',
-    caption: longString,
+    caption: "One of the university’s class projects. It let users track their easily-lost items. The application supports iOS devices, providing the interactive UI, which lets them see the items’ location in real-time and can provide the route to the lost items.",
     imageUrl: FostSrc,
     categories: [Category.App],
 
-    details: [longString, longString],
+    details: [unavailableText],
     tools: ['Swift', 'NodeJS'],
     images: [
         { label: 'Fost', imageUrl: FostSrc },
@@ -269,11 +269,11 @@ const fost: ShowcaseData = {
 
 const bonderBoard: ShowcaseData = {
     title: 'Bonder Board, Chemistry Puzzle RPG Mobile Game',
-    caption: longString,
+    caption: "A puzzle RPG mobile game features chemistry-based logic in the gameplay and cute characters inspired by real-life chemistry elements. The game supported both Android and iOS platforms and was published on the Play Store (currently discontinued).",
     imageUrl: BonderBoardSrc,
     categories: [Category.Game],
 
-    details: [longString, longString],
+    details: [unavailableText],
     tools: ['C#', 'Unity'],
     images: [
         { label: 'Bonder Board', imageUrl: BonderBoardSrc },
